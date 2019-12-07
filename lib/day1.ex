f81bbdcde5af06986d9e46e15d3c346eab96fdea
David Sullins <david.sullins@gmail.com>
2019-12-06 19:20:00 -0600

    Part 2: add code to parse and sum total fuel costs

--- lib/day1.ex
+++ lib/day1.ex
@@ -5,6 +5,8 @@ defmodule Day1 do
   def day1 do
     output = parse_file("_input/input1.txt")
     IO.puts("part 1: Total fuel required #{output}")
+    output2 = parse_file2("_input/input1.txt")
+    IO.puts("part 2: Total fuel required #{output2}")
   end
 
   def count_up_fuel(mass) do
@@ -29,5 +31,13 @@ defmodule Day1 do
     else
       0
     end
+  end
+
+  def parse_file2(filename) do
+    File.stream!(filename)
+    |> Stream.map(&String.trim_trailing/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.map(&Day1.count_up_fuel2/1)
+    |> Enum.sum()
   end
 end
