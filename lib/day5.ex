cccdce58d35df9a5823f7b992ca56f7958c9de9e
David Sullins <david.sullins@gmail.com>
2019-12-17 17:28:21 -0600

    Day 5 part 2 input/output

--- lib/day5.ex
+++ lib/day5.ex
@@ -8,5 +8,9 @@ defmodule Day5 do
     {_, outputs} = IntCode.exec_intcode(input, [1])
 
     IO.puts(["Day5 part 1 outputs: ", Enum.join(outputs, ", ")])
+
+    # part 2
+    {_, outputs2} = IntCode.exec_intcode(input, [5])
+    IO.puts(["Day5 part 2 outputs: ", Enum.join(outputs2, ", ")])
   end
 end
